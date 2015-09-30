4e7dc14fd577d7a6090b83239ee02e5afc96ed27
Hristo Erinin (Pythian) <erinin@pythian.com>
2015-09-30 05:14:16 -0600

    Add additional documentation about the collector
     * Add instructions to grant the lowest amount of privileges.
     * Describe configuration options
     * Add .gitignore to the repository
     * remove Python Debugger
     * Update metrics 'path' to be more specific

--- mysqldbsizes.py
+++ mysqldbsizes.py
@@ -2,12 +2,65 @@
 
 """
 
-Diamond collector that monitors MySQL table sizes 
+Diamond collector that monitors MySQL table sizes
 
 #### Dependencies
 
  * MySQLdb
  * MySQL 5.0.3+
+
+### Grants and Privileges
+
+MySQL filters the information visible from `information_schema` tables based
+on the access privileges of the user executing the query. Information about
+database objects that the user is not permitted to access is hidden. To get
+access to information about all tables from `information_schema` the MySQL
+user needs at least `REFERENCES` privilege on the database (`*.*`).
+
+In versions of MySQL before 5.5.41, 5.6.22, and 5.7.6, the `REFERENCES`
+privilege is marked as 'unused'. After these versions the privilege gives
+additional access. If `REFERENCES` privilege is unavailable you will need to
+grant access (`SELECT`) to the databases and tables.
+
+Example:
+
+    -- Create user for statistics gathering:
+    CREATE USER `stats`@`localhost` IDENTIFIED BY `somerandompassword`;
+    -- Grant access to information about tables and databases from information_schema schema
+    GRANT REFERENCES ON *.* TO 'stats'@'localhost';
+
+    -- Grant SELECT on all tables from specific databases
+    -- GRANT SELECT on database_prod1.* to `stats`@`localhost`;
+    -- GRANT SELECT on database_prod2.* to `stats`@`localhost`;
+
+#### Customizing
+
+Diamond looks for collectors in /usr/lib/diamond/collectors/ (on Ubuntu). By
+default diamond will invoke the *collect* method every 60 seconds.
+
+You can put a section named `[[MySQLSizeCollector]]` under `[collectors]` in
+your diamond.conf:
+
+	[collectors]
+	[[MySQLSizeCollector]]
+	# enable the collector
+	enabled = True
+	# no need to gather statistics more frequently
+	interval = 600
+	
+
+Diamond collectors that require a separate configuration file should place a
+.conf file in /etc/diamond/collectors/.
+The configuration file name should match the name of the diamond collector
+class. Configuration file for this collector should be named `MySQLSizeCollector.conf`
+/etc/diamond/collectors/MySQLSizeCollector.conf.
+
+Example configuration file:
+
+    host = localhost
+    user = stat
+    password = somerandompassword
+
 
 """
 
@@ -44,7 +97,7 @@ class MySQLSizeCollector(diamond.collector.Collector):
         """
         config = super(MySQLSizeCollector, self).get_default_config()
         config.update({
-            'path':     'mysql',
+            'path':     'mysql.size',
             # Connection settings
             'host': 'localhost',
             'port': 3306,
@@ -57,8 +110,6 @@ class MySQLSizeCollector(diamond.collector.Collector):
         return config
 
     def get_db_results(self, query):
-        import pdb
-#        pdb.set_trace()
         cursor = self.db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
 
         try:
@@ -102,7 +153,7 @@ class MySQLSizeCollector(diamond.collector.Collector):
                 metrics[metric_name] = row
         except (AttributeError, MySQLError), e:
             self.log.error('%s: could not get table sizes: %s', self.__class__.__name__, e)
-            raise e 
+            raise e
         return metrics
 
     def disconnect(self):
@@ -134,10 +185,10 @@ class MySQLSizeCollector(diamond.collector.Collector):
             raise e
 
 
-        for name in metrics:
+        for name in metrics.keys():
             self.log.debug('%s: publishing metrics for %s', self.__class__.__name__, name)
             for metric, value in metrics[name].items():
                 if metric in ('table_schema','table_name'):
-                    continue 
+                    continue
                 self.log.debug('%s: publish for %s: %s=%s', self.__class__.__name__, name, metric, value)
                 self.publish(name + "." + metric, value)
